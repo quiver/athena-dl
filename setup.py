8f40bc47c064e0b6e467cb13009f45acea9fc840
George Yoshida <dynkin@gmail.com>
2018-01-26 12:54:05 +0100

    fix typo

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     ],
    classifiers=(
        'Development Status :: 3 - Alpha',
-       'License :: OSI Approved :: MIT License'
+       'License :: OSI Approved :: MIT License',
        'Programming Language :: Python :: 2.7',
    ),
     entry_points='''
